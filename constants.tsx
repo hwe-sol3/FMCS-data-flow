e394216b5520f96b5a9f751610c7d62fbb23c010
hwe-sol3 <hwe.sol3@gmail.com>
2025-12-18 15:22:22 +0900

    [Create] 최초 등록
    Define constants for site information and application title.

---
+++ constants.tsx
@@ -0,0 +1,31 @@
+import { Site } from './types';
+
+export const SITES: Site[] = [
+  {
+    id: 'cheonan',
+    name: '천안',
+    subMenus: ['UPW', 'WWT'],
+  },
+  {
+    id: 'onyang',
+    name: '온양',
+    subMenus: ['UPW'],
+  },
+  {
+    id: 'asan-green',
+    name: '아산그린센터',
+    subMenus: ['WWT'],
+  },
+  {
+    id: 'asan-1',
+    name: '아산1',
+    subMenus: ['UPW'],
+  },
+  {
+    id: 'asan-2',
+    name: '아산2',
+    subMenus: ['UPW'],
+  },
+];
+
+export const APP_TITLE = "FMCS Data Flow System";
