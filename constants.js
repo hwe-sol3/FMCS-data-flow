c6436a97d149c4bde6bac435f8204b2269bf2d35
hwe-sol3 <hwe.sol3@gmail.com>
2025-12-18 15:53:04 +0900

    [Create] 최초 등록

---
+++ constants.js
@@ -0,0 +1,29 @@
+export const SITES = [
+  {
+    id: 'cheonan',
+    name: '천안',
+    subMenus: ['UPW', 'WWT'],
+  },
+  {
+    id: 'onyang',
+    name: '온양',
+    subMenus: ['UPW'],
+  },
+  {
+    id: 'asan-green',
+    name: '아산그린센터',
+    subMenus: ['WWT'],
+  },
+  {
+    id: 'asan-1',
+    name: '아산1',
+    subMenus: ['UPW'],
+  },
+  {
+    id: 'asan-2',
+    name: '아산2',
+    subMenus: ['UPW'],
+  },
+];
+
+export const APP_TITLE = "FMCS Data Flow System";
